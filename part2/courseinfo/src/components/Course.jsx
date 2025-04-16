b024a4564b2981ac6749572b5f3a086848f39f6c
Tariq-Khalidee <tariq.khalidee@gmail.com>
2025-04-16 10:23:09 +0200

    Part 2 map created. Part.2.1: courseinfo done!

---
+++ part2/courseinfo/src/components/Course.jsx
@@ -0,0 +1,27 @@
+const Header = (props) => <h1>{props.course}</h1>
+
+const Content = (props) => (
+  <div>
+    <Part part={props.parts[0]} />
+    <Part part={props.parts[1]} />
+    <Part part={props.parts[2]} />
+  </div>
+)
+
+const Part = (props) => (
+  <p>
+    {props.part.name} {props.part.exercises}
+  </p>
+)
+
+
+const Course = (props) => {
+    return (
+        <div>
+            <Header course={props.course.name}/>
+            <Content parts={props.course.parts}/>
+        </div>
+    )
+}
+
+export default Course 
